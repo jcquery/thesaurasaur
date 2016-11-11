84aac27b50afde4f1f18aad97a74280ec053918b
John Carney <jcarney000@gmail.com>
2016-11-11 15:46:51 -0800

    integrate text analysis api

--- routes/parse.js
+++ routes/parse.js
@@ -3,21 +3,27 @@
 const express = require('express');
 const pos = require('pos');
 const axios = require('axios');
-const key = process.env.API_KEY;
+const tKey = process.env.T_API_KEY;
+const pKey = process.env.P_API_KEY;
 const router = express.Router();
 
 router.post('/parse', (req, res, next) => {
   const text = req.body.text;
 
-  if (!text) {
-    return res.status(400).send('No text!');
-  }
-
-  const words = new pos.Lexer().lex(text);
-  const tagger = new pos.Tagger();
-  const taggedWords = tagger.tag(words);
-  
-  res.send(taggedWords);
+  axios.post('https://textanalysis.p.mashape.com/nltk-pos-tagging', `text=${text}`, {
+    headers: {
+      "X-Mashape-Key": "vaHegy10Qbmsh7DoQNenVN0gmVmap12hzs4jsnhflVWwFae6xG",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json"
+    }
+  })
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((err) => {
+    console.error(err);
+    next(err);
+  });
 });
 
 module.exports = router;
